9c315ae893c37f91c4003ceb991c80a6d65369a0
Max1mus7 <maxdweber7@gmail.com>
2022-04-01 14:50:26 -0700

    update devicerecords to use connection pools

--- data/deviceRecordsDAO.js
+++ data/deviceRecordsDAO.js
@@ -21,25 +21,25 @@ const config = {
     },
 };
 
-
+const pool = new mssql.ConnectionPool(config);
 //const helper = new HelperMethods();
 
 class deviceRecordsDAO {
     
     async connect() {
-        await mssql.connect(config);
+        await pool.connect();
     }
 
     async getAll() {
         let resultObj;
         try {
             await this.connect();
-            const result = await mssql.query`SELECT * FROM presto1.device_records`;
+            const result = await pool.query`SELECT * FROM presto1.device_records`;
             resultObj = {code: 200, queryResult: result.recordsets[0]};
         } catch (err) {
             resultObj = {code: 500, message: err.message};
         } finally {
-            await mssql.close();
+            //await mssql.close();
             return resultObj;
         }
     }
@@ -48,12 +48,12 @@ class deviceRecordsDAO {
         let resultObj;
         try {
             await this.connect();
-            const result = await mssql.query`SELECT * FROM presto1.device_records WHERE Id = ${id}`;
+            const result = await pool.query`SELECT * FROM presto1.device_records WHERE Id = ${id}`;
             resultObj = {code: 200, queryResult: result.recordsets[0]};
         } catch (err) {
             resultObj = {code: 500, message: err.message};
         } finally {
-            await mssql.close();
+            //await mssql.close();
             return resultObj;
         }
     }
@@ -62,12 +62,12 @@ class deviceRecordsDAO {
         let resultObj;
         try{
             await this.connect();
-            const result = await mssql.query`SELECT TOP 1 * FROM presto1.device_records WHERE OwnerId = ${id} ORDER BY CreatedAt DESC`;
+            const result = await pool.query`SELECT TOP 1 * FROM presto1.device_records WHERE OwnerId = ${id} ORDER BY CreatedAt DESC`;
             resultObj = {code: 200, queryResult: result.recordsets[0]};
         } catch (err) {
             resultObj = {code: 500, message: err.message};
         } finally {
-            await mssql.close();
+            //await mssql.close();
             return resultObj;
         }
     }
@@ -76,7 +76,7 @@ class deviceRecordsDAO {
         let resultObj;
         try{
             await this.connect();
-            const result = await mssql.query`SELECT TOP 20 * FROM presto1.device_records WHERE OwnerId = ${id} ORDER BY CreatedAt DESC`;
+            const result = await pool.query`SELECT TOP 20 * FROM presto1.device_records WHERE OwnerId = ${id} ORDER BY CreatedAt DESC`;
             DataValidation.isEmpty(result.recordsets[0]);
             resultObj = {code: 200, queryResult: result.recordsets[0]};
         } catch (err) {
@@ -88,7 +88,7 @@ class deviceRecordsDAO {
                 resultObj = {code: 500, message: err.message};
             }
         } finally {
-            await mssql.close();
+            //await mssql.close();
             console.log(resultObj);
             return resultObj;
         }
@@ -98,12 +98,12 @@ class deviceRecordsDAO {
         let resultObj;
         try{
             await this.connect();
-            const result = await mssql.query`SELECT * FROM presto1.device_records WHERE OwnerId = ${id} ORDER BY CreatedAt DESC`;
+            const result = await pool.query`SELECT * FROM presto1.device_records WHERE OwnerId = ${id} ORDER BY CreatedAt DESC`;
             resultObj = {code: 200, queryResult: result.recordsets[0]};
         } catch (err) {
             resultObj = {code: 500, message: err.message};
         } finally {
-            await mssql.close();
+            //await mssql.close();
             return resultObj;
         }
     }
@@ -112,12 +112,12 @@ class deviceRecordsDAO {
         let resultObj;
         try{
             await this.connect();
-            const result = await mssql.query`SELECT * FROM presto1.device_records WHERE ParentDevice = ${id}`;
+            const result = await pool.query`SELECT * FROM presto1.device_records WHERE ParentDevice = ${id}`;
             resultObj = {code:200, queryResult: result.recordsets[0]};
         } catch(err) {
             resultObj = {code: 500, message: err.message};
         } finally {
-            await mssql.close();
+            //await mssql.close();
             return resultObj;
         }
     }
@@ -128,12 +128,12 @@ class deviceRecordsDAO {
             await this.connect();
             let location = body.location;
             console.log(location);
-            await mssql.query`INSERT INTO presto1.device_records (ParentDevice, OwnerId, ReportedLost, DeviceLatitude, DeviceLongitude) VALUES (${body.parent_device}, ${body.owner_id}, ${body.reported_lost}, ${location.latitude+''},${location.longitude+''})`;
+            await pool.query`INSERT INTO presto1.device_records (ParentDevice, OwnerId, ReportedLost, DeviceLatitude, DeviceLongitude) VALUES (${body.parent_device}, ${body.owner_id}, ${body.reported_lost}, ${location.latitude+''},${location.longitude+''})`;
             resultObj = {code: 201, message: 'Successfully added device record to DB'};
         } catch (err) {
             resultObj = {code: 500, message: err.message};
         } finally {
-            await mssql.close();
+            //await mssql.close();
             return resultObj;
         }
     }
@@ -142,12 +142,12 @@ class deviceRecordsDAO {
         let resultObj;
         try{
             await this.connect();
-            await mssql.query`DELETE FROM presto1.device_records WHERE Id = ${id}`;
+            await pool.query`DELETE FROM presto1.device_records WHERE Id = ${id}`;
             resultObj = {code: 204, message: 'Successfully removed device record from DB'};
         } catch (err) {
             resultObj = {code: 500, message: err.message};
         } finally {
-            await mssql.close();
+            //await mssql.close();
             return resultObj;
         }
     }
